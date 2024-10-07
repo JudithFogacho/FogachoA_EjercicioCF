2975ca5753b952b45ee0c05207cca9eb5cc9e31a
laboratorio <laboratorio@UPEAULA-31721>
2024-10-07 11:18:23 -0500

    Agregar archivos de proyecto.

---
+++ Data/FogachoA_EjercicioCFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FogachoA_EjercicioCF.Models;
+
+namespace FogachoA_EjercicioCF.Data
+{
+    public class FogachoA_EjercicioCFContext : DbContext
+    {
+        public FogachoA_EjercicioCFContext (DbContextOptions<FogachoA_EjercicioCFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FogachoA_EjercicioCF.Models.Burguer> Burguer { get; set; } = default!;
+    }
+}
